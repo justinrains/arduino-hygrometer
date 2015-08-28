dfdf2c7a55545ae101a625b9f901b024f60b662d
Justin Rains <justin@justinrains.com>
2015-08-28 17:23:46 -0400

    Initial code check in.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # arduino-hygrometer
+By Justin Rains, http://www.justinrains.com/ , http://justinsresume.com/
+This is where I'll update the code for my Arduino-hygrometer project.
---
+++ hygrometer.ino
@@ -0,0 +1,15 @@
+//const int aIn = 
+
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+
+void loop() {
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(0);
+  Serial.println(sensorValue);
+  delay(3000);
+}
+
